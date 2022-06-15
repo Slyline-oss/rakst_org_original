d50cbdc03884c73fce5be47a8f422c219c38f7d4
start.vaadin.com <no-reply@vaadin.com>
2022-06-15 09:35:47 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/profile/ProfileView.java
@@ -0,0 +1,37 @@
+package com.example.application.views.profile;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@PageTitle("Profile")
+@Route(value = "profile", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@AnonymousAllowed
+public class ProfileView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public ProfileView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}

349461167c482d39a1234731c9667461fddfbddf
Sergejs Roze <sergejs.roze@posti.com>
2023-09-18 17:16:05 +0300

    Fixed missing code on commit

--- src/main/java/org/raksti/web/data/service/TextRepository.java
+++ src/main/java/org/raksti/web/data/service/TextRepository.java
@@ -3,6 +3,8 @@ package org.raksti.web.data.service;
 import org.jetbrains.annotations.NotNull;
 import org.raksti.web.data.entity.Text;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,4 +16,7 @@ public interface TextRepository extends JpaRepository<Text, String> {
 
     @NotNull Optional<Text> findById(@NotNull String id);
 
+    @Modifying
+    @Query("update Text t set t.content_1 = ?1, t.content_2 = ?2, t.content_3 = ?3  where t.id = ?4")
+    void setContentById(String content_1, String content_2, String content_3, String id);
 }

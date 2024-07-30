e6711004a254140a56cc01c17de4acb578f9b3f6
Sergejs Roze <sergejs.roze@posti.com>
2024-07-30 10:42:16 +0300

    Updated logo

--- import.sql
+++ import.sql
@@ -1,1 +1,2 @@
-ALTER TABLE application_user ADD COLUMN IF NOT EXISTS offline_note VARCHAR(255);
+ALTER TABLE application_user ADD COLUMN IF NOT EXISTS allow_emails BOOLEAN;
+UPDATE application_user SET allow_emails = TRUE;
